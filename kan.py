15b371569cbf44296acad6a906b1a88ed0fea2a7
wynxel <turzamarek.tm@gmail.com>
2022-02-24 21:38:47 +0100

    kontroluj implemented

--- kan.py
+++ kan.py
@@ -8,8 +8,14 @@ def utried(possible):
     lens = {k: len(v) for k, v in possible.items()}
     return {k: possible[k] for k, _ in sorted(lens.items(), key=lambda item: item[1])}
 
-def kontroluj(possible):
-    # T: zoznamy su nenulovej dlzky, F: inac
-    pass
 
-utried({"A": [3,2,1], "B":[2], "C":[2], "X": [3,4,5,6], "w":[]})
+def kontroluj(possible):
+    return len(possible) > 0 and len(list(possible.values())[0]) > 0
+
+
+a = utried({"A": [3,2,1], "B":[2], "C":[2], "X": [3,4,5,6], "w":[]})
+kontroluj(a) # flase
+del a['w']
+kontroluj(a) # true
+kontroluj({}) # flase
+
