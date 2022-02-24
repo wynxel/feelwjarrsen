99c43aa4bf67412a372fec2a2aea40fa5448c41a
jfhub11 <julia.froncova@gmail.com>
2022-02-24 21:58:38 +0100

    'tak'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from kan import utried, kontroluj, odstran
 
 contribs, projects = {"Anna": {"C++": 2}}, {"WebServer": {"DAY": 7, "SCR": 10, "BBF":7, "ROLE": {"HTML": 3, "C++": 2}}}
 output = []
@@ -13,37 +14,36 @@ for key, value in contribs.items():
             linkedin[key2][key] = value2
 
 
-for project in projects:
-    possible = {role: [] for role in project.keys()}
-    is_possible = True
-    for role, role_level in project["ROLE"].items():
-        if role in linkedin:
-            for name, level in linkedin[role].items():
-                if level >= role_level:
-                    possible[role].append(name)
-        if len(possible[role]) == 0:
-            is_possible = False
-            break
-    if not is_possible:
-        continue
-    possible_tmp = {k: len(v) for k, v in possible.items()}
-    possible_lens = {k: v for k, v in sorted(possible_tmp.items(), key=lambda item: item[1])}
-    result = set()
-    skip = False
-    for skill, num in possible_lens.items():
-        if num == 1:
-            if possible[skill][0] in result:
-                skip = True
+def get_one_full_project():
+    for project in projects:
+        possible = {role: [] for role in project.keys()}
+        is_possible = True
+        for role, role_level in project["ROLE"].items():
+            if role in linkedin:
+                for name, level in linkedin[role].items():
+                    if level >= role_level:
+                        possible[role].append(name)
+            if len(possible[role]) == 0:
+                is_possible = False
                 break
-            result.add(possible[skill][0])
-        else:
+        if not is_possible:
+            continue
+        people = set()
 
-    if skip:
-        continue
-    possible = odstran(possible, "Meno cloveka") #
-    possible = utried(possible) # kluce (role) su utriedene vzostupne podla poctu ludi ktori ich vedia
-    kontroluj(possible) # T: zoznamy su nenulovej dlzky, F: inac
-
+        broken = False
+        while possible.__len__() != 0:
+            possible = utried(possible)
+            if not kontroluj():
+                broken = True
+                break
+            first = list(possible.items())[0]
+            first_person = first[1][0]
+            first_role = first[0]
+            possible = odstran(possible, first_person, first_role)
+            people.add(first_person)
+        if not broken:
+            return project, people
+    return False
 
 
     # ma possible vsetky role vyplnene?
