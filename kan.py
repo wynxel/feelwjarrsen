6d98fa84320800125dbfba7cf0e42416e015e9e8
wynxel <turzamarek.tm@gmail.com>
2022-02-24 21:32:30 +0100

    utried implemented

--- kan.py
+++ kan.py
@@ -1,11 +1,15 @@
+possible = {role: [] for role in project.keys()}
+
 
 def odstran(possible, name):
     pass
 
 def utried(possible):
-    # kluce (role) su utriedene vzostupne podla poctu ludi ktori ich vedia
-    pass
+    lens = {k: len(v) for k, v in possible.items()}
+    return {k: possible[k] for k, _ in sorted(lens.items(), key=lambda item: item[1])}
 
 def kontroluj(possible):
     # T: zoznamy su nenulovej dlzky, F: inac
     pass
+
+utried({"A": [3,2,1], "B":[2], "C":[2], "X": [3,4,5,6], "w":[]})
