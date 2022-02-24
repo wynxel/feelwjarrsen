821cd43c1c6f99583a3c757aba59ed126d03590a
Jany <jpastorek5@gmail.com>
2022-02-24 20:20:38 +0100

    g

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from collections import defaultdict
-import input
 
-#contribs, projects = {"Anna": {"C++": 2}}, {"WebServer": {"DAY": 7, "SCR": 10, "BBF":7, "ROLE": {"HTML": 3, "C++": 2}}}
-contribs, projects = input.load(), {}
+contribs, projects = {"Anna": {"C++": 2}}, {"WebServer": {"DAY": 7, "SCR": 10, "BBF":7, "ROLE": {"HTML": 3, "C++": 2}}}
 output = []
 
 # reverse the dictionary
@@ -14,18 +12,12 @@ for key, value in contribs.items():
         else:
             linkedin[key2][key] = value2
 
+
 for project in projects:
     possible = defaultdict(lambda: [])
     for role, role_level in project["ROLE"].items():
         if role in linkedin:
             for name, level in linkedin[role].items():
                 if level >= role_level:
-                    # {"C++": ["meno1", ...]}
                     possible[role].append(name)
-    # ma possible vsetky role vyplnene?
-    # ci nie je jeden na dve role ktore nevie nik iny?
-    # ak je vsetko ok, pridaj do vysledku
-
-    # pridat do output
-
 
