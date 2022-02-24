62eb820039c9da63bbdfd04b7b8558a27c9e131c
wynxel <turzamarek.tm@gmail.com>
2022-02-24 20:02:59 +0100

    marek

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+from collections import defaultdict
 
-
-colab, projects = {}, {"WebServer": {"DAY": 7, "SCR": 10, "BBF"7, "ROLE": {"HTML": 3, "C++": 2}}}
+contribs, projects = {"Anna": {"C++": 2}}, {"WebServer": {"DAY": 7, "SCR": 10, "BBF":7, "ROLE": {"HTML": 3, "C++": 2}}}
 output = []
 
+# reverse the dictionary
+linkedin = {}
+for key, value in contribs.items():
+    for key2, value2 in value.items():
+        if key2 not in linkedin:
+            linkedin[key2] = {key: value2}
+        else:
+            linkedin[key2][key] = value2
+
 for project in projects:
+    possible = defaultdict(lambda: [])
+    for role, role_level in project["ROLE"].items():
+        if role in linkedin:
+            for name, level in linkedin[role].items():
+                if level >= role_level:
+                    possible[role].append(name)
 
