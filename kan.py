2473aa61ab575e36f048e0bb340f997014862e97
wynxel <turzamarek.tm@gmail.com>
2022-02-24 21:22:51 +0100

    zufale

---
+++ kan.py
@@ -0,0 +1,11 @@
+
+def odstran(possible, name):
+    pass
+
+def utried(possible):
+    # kluce (role) su utriedene vzostupne podla poctu ludi ktori ich vedia
+    pass
+
+def kontroluj(possible):
+    # T: zoznamy su nenulovej dlzky, F: inac
+    pass
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for key, value in contribs.items():
 
 
 for project in projects:
-    possible = defaultdict(lambda: [])
+    possible = {role: [] for role in project.keys()}
     is_possible = True
     for role, role_level in project["ROLE"].items():
         if role in linkedin:
@@ -23,7 +23,27 @@ for project in projects:
                     possible[role].append(name)
         if len(possible[role]) == 0:
             is_possible = False
-    if is_possible:
+            break
+    if not is_possible:
+        continue
+    possible_tmp = {k: len(v) for k, v in possible.items()}
+    possible_lens = {k: v for k, v in sorted(possible_tmp.items(), key=lambda item: item[1])}
+    result = set()
+    skip = False
+    for skill, num in possible_lens.items():
+        if num == 1:
+            if possible[skill][0] in result:
+                skip = True
+                break
+            result.add(possible[skill][0])
+        else:
+
+    if skip:
+        continue
+    possible = odstran(possible, "Meno cloveka") #
+    possible = utried(possible) # kluce (role) su utriedene vzostupne podla poctu ludi ktori ich vedia
+    kontroluj(possible) # T: zoznamy su nenulovej dlzky, F: inac
+
 
 
     # ma possible vsetky role vyplnene?
